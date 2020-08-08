9643e95eb5f7a7a93e7bfa317de2c9decff65846
Kirill Brodt <cyrill.brodt@gmail.com>
2020-08-08 01:54:03 +0000

    Add promt to zsh

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -20,6 +20,29 @@ setopt extendedhistory
 # Time to wait for additional characters in a sequence
 KEYTIMEOUT=1 # corresponds to 10ms
 
+# Use vim style navigation keys in menu completion
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
+
+# Use vim as the editor
+export EDITOR=vim
+
+# Use vim style line editing in zsh
+bindkey -v
+# Movement
+bindkey -a 'gg' beginning-of-buffer-or-history
+bindkey -a 'G' end-of-buffer-or-history
+# Undo
+bindkey -a 'u' undo
+bindkey -a '^R' redo
+# Edit line
+bindkey -a '^V' edit-command-line
+# Backspace
+bindkey '^?' backward-delete-char
+bindkey '^H' backward-delete-char
+
 # Use incremental search
 bindkey "^R" history-incremental-search-backward
 

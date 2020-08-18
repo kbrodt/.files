b55dc51da7efdf66e6defe878d95b19ff2308aa2
Kirill Brodt <cyrill.brodt@gmail.com>
2020-08-18 05:58:07 +0000

    Refactor install deps for nvim

--- neovim/install_deps.sh
+++ neovim/install_deps.sh
@@ -4,13 +4,12 @@
 set -o nounset    # error when referencing undefined variable
 set -o errexit    # exit when command fails
 
-
 installubuntu() {
     sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
 }
 
 installdeps() {
-    [  -n "$(uname -a | grep Ubuntu)" ] && installubuntu
+    (uname -a | grep -q Ubuntu) && installubuntu
 }
 
 installdeps

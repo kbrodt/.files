322ac0c2f9c5c216d3c30696a58e037766b678ad
Kirill Brodt <cyrill.brodt@gmail.com>
2020-08-18 10:54:18 -0400

    Refactor pyide

--- pyide/install.sh
+++ pyide/install.sh
@@ -4,9 +4,4 @@
 set -o nounset    # error when referencing undefined variable
 set -o errexit    # exit when command fails
 
-python3 -m pip install --upgrade pip --user
-python3 -m pip install --upgrade jedi --user
-python3 -m pip install --upgrade black --user
-python3 -m pip install --upgrade rope --user
-python3 -m pip install --upgrade flake8 --user
-python3 -m pip install --upgrade mypy --user
+python3 -m pip install --upgrade --user -r requirements.txt

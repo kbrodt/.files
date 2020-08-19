20cfffa9fac6fb115ae75c995d4f8359fd6b4fc3
Kirill Brodt <cyrill.brodt@gmail.com>
2020-08-19 06:22:08 +0000

    Add more bash aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,16 +1,52 @@
 # Use neovim for vim if present.
 [ -x "$(command -v nvim)" ] && alias vim="nvim" vimdiff="nvim -d"
 
-# Verbosity and settings that you pretty much just always are going to want.
+### ARCHIVE EXTRACTION
+# usage: ex <file>
+ex ()
+{
+  if [ -f $1 ] ; then
+    case $1 in
+      *.tar.bz2)   tar xjf $1   ;;
+      *.tar.gz)    tar xzf $1   ;;
+      *.bz2)       bunzip2 $1   ;;
+      *.rar)       unrar x $1   ;;
+      *.gz)        gunzip $1    ;;
+      *.tar)       tar xf $1    ;;
+      *.tbz2)      tar xjf $1   ;;
+      *.tgz)       tar xzf $1   ;;
+      *.zip)       unzip $1     ;;
+      *.Z)         uncompress $1;;
+      *.7z)        7z x $1      ;;
+      *.deb)       ar x $1      ;;
+      *.tar.xz)    tar xf $1    ;;
+      *.tar.zst)   unzstd $1    ;;
+      *)           echo "'$1' cannot be extracted via ex()" ;;
+    esac
+  else
+    echo "'$1' is not a valid file"
+  fi
+}
+
+# settings that you pretty much just always are going to want.
 alias \
-	cp="cp -iv" \
-	mv="mv -iv" \
-	rm="rm -vI" \
-	mkd="mkdir -pv" \
+	cp="cp -i" \
+	mv="mv -i" \
+	rm="rm -I" \
+	mkd="mkdir -p" \
+    df="df -h" \
+
+# navigation
+alias \
+    ..="cd .." \
+    ...="cd ../.." \
+    .3="cd ../../.." \
 
 # Colorize commands when possible.
 alias \
+	la="ls -a --color=auto --group-directories-first" \
+	ll="ls -l --color=auto --group-directories-first" \
+	l.='ls -a | grep "^\."' \
 	ls="ls -hN --color=auto --group-directories-first" \
-	ll="ls -lah" \
 	grep="grep --color=auto" \
 	diff="diff --color=auto" \

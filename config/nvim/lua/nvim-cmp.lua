1548c82fc5388d34b79a65cc41e3f0c4b9fa5073
Kirill Brodt <cyrill.brodt@gmail.com>
2021-11-10 17:54:52 -0500

    Update nvim-cmp keybindings

--- config/nvim/lua/nvim-cmp.lua
+++ config/nvim/lua/nvim-cmp.lua
@@ -14,8 +14,6 @@ cmp.setup {
       autocomplete = false
   },
   mapping = {
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
@@ -24,26 +22,29 @@ cmp.setup {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-    ['<Tab>'] = function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
+    -- ['<Tab>'] = function(fallback)
       -- if cmp.visible() then
         -- cmp.select_next_item()
-      else
-        fallback()
-      end
-    end,
-    ['<S-Tab>'] = function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-p>', true, true, true), 'n')
+      -- else
+        -- fallback()
+      -- end
+    -- end,
+    -- ['<S-Tab>'] = function(fallback)
       -- if cmp.visible() then
         -- cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end,
+      -- else
+        -- fallback()
+      -- end
+    -- end,
   },
   sources = {
+    { name = 'nvim_lua' },
     { name = 'nvim_lsp' },
+  },
+
+  experimental = {
+    native_menu = false,
+
+    ghost_text = true,
   },
 }

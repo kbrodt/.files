aff8f47129f9bf79d1b0292e581a88b72fdae21e
Kirill Brodt <cyrill.brodt@gmail.com>
2022-05-07 22:10:23 -0400

    Update vim plugins

--- config/nvim/lua/nvim-cmp.lua
+++ config/nvim/lua/nvim-cmp.lua
@@ -12,21 +12,20 @@ cmp.setup {
   completion = {
       autocomplete = false
   },
-  mapping = {
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-  },
-  sources = {
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-  },
+  }),
 
   experimental = {
     native_menu = false,

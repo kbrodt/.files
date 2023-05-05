fa13fd46cb02b2e5169ac11a491b921278ca392e
Kirill Brodt <cyrill.brodt@gmail.com>
2023-05-05 00:21:39 -0400

    Update nvim lsp configs

--- config/nvim/lua/nvim-cmp.lua
+++ config/nvim/lua/nvim-cmp.lua
@@ -17,11 +17,15 @@ cmp.setup {
     -- documentation = cmp.config.window.bordered(),
   },
   mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+    ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
+    -- C-b (back) C-f (forward) for snippet placeholder navigation.
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },

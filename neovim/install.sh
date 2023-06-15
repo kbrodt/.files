6018b612490d7ff428148abb934059fcd6f53c40
Kirill Brodt <cyrill.brodt@gmail.com>
2023-06-15 13:55:43 -0400

    Add telescope required deps

--- neovim/install.sh
+++ neovim/install.sh
@@ -41,10 +41,12 @@ for repo in \
     https://github.com/hrsh7th/cmp-nvim-lsp-signature-help.git \
     https://github.com/hrsh7th/cmp-path \
     https://github.com/nvim-treesitter/nvim-treesitter.git \
+    https://github.com/nvim-lua/plenary.nvim.git \
     https://github.com/nvim-telescope/telescope.nvim.git \
     https://github.com/mg979/vim-visual-multi.git \
     ; do
-    repo_dir=$(basename "${repo}" | cut -d'.' -f 1)
+    repo_dir=$(basename "${repo}" | sed "s/\.git$//")
+    echo $repo_dir
     if [ -d "${repo_dir}" ]; then
         cd "${repo_dir}" && git pull && cd ..
     else

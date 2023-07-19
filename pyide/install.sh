2c333fce2049c3d295899f6fb89b3d0ffece5d6f
Kirill Brodt <cyrill.brodt@gmail.com>
2023-07-18 21:23:49 -0400

    Add break system packages for pip

--- pyide/install.sh
+++ pyide/install.sh
@@ -4,6 +4,6 @@
 set -o nounset    # error when referencing undefined variable
 set -o errexit    # exit when command fails
 
-python3 -m pip install --upgrade --user \
+python3 -m pip install --break-system-packages --upgrade --user \
     pip black rope flake8 mypy pynvim pylint isort \
     cpplint clang-format
